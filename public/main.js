6f035eeaf924dac4167fa3b3b7a7155d74f1ab45
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-10-19 12:59:14 -0500

    listo

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 let baseDatosIndividual = []
-const DOMINIO = 'https://primera-version.herokuapp.com'
-// const DOMINIO = 'https://aplicacion-de-mensajes-production.up.railway.app/'
+// const DOMINIO = 'https://primera-version.herokuapp.com'
+const DOMINIO = 'https://aplicacion-de-mensajes-production.up.railway.app'
 // const DOMINIO = 'http://localhost:3000'
 ///////////////////////////////////////////////////////////////
 const contenedorMensajes = document.querySelector('#contenedorMensajes')
