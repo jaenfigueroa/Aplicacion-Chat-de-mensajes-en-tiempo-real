95c259296452139068e58058006815a8c8768ec8
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-10-20 04:13:07 -0500

    backend terminado

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ var dias = [
   'lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'
 ]
 
+////////////////////////////////////////////////////////////////////
 const baseDatos = require('../baseDatos/baseDatos.json')
+const usuarios = require('../baseDatos/usuarios.json')
 
 //GUARDAR MENSAJES NUEVOS///////////////////////////////////////////
 server.post('/enviarMensaje', (req, res) => {
@@ -60,6 +62,91 @@ function fechaActual() {
 
   return `${hora}:${minutos} - ${dia}, ${fecha} de ${mes}`
 }
+
+//REGISTRAR NUEVO USUARIO/////////////////////////////////////////////
+server.post('/registrate', (req, res) => {
+  let nombreUsuario = req.body.nombre || 'Pepe'
+  let passwordUsuario = req.body.password || 'noname'
+  let idUsuario = Math.floor(Math.random() * 10000000000)
+
+  let numeroUsuario = {
+    id: idUsuario,
+    nombre: nombreUsuario,
+    password: passwordUsuario,
+  }
+
+  let resultado = usuarios.lista.some(x => x.nombre == nombreUsuario)
+
+  if (resultado) {
+    res.send('El usuario ya existe')
+  } else {
+    usuarios.lista.push(numeroUsuario)
+    fs.writeFileSync('./baseDatos/usuarios.json', JSON.stringify(usuarios))
+
+    res.send('Usuario creado con exito')
+  }
+
+})
+
+//INICIAR SESION///////////////////////////////////////////////////////
+server.post('/iniciarSesion', (req, res) => {
+  let nombreUsuario = req.body.nombre || ''
+  let passwordUsuario = req.body.password || ''
+
+  //comprobar si el usuario existe
+  let existencia = usuarios.lista.some(x => x.nombre === nombreUsuario)
+
+  //comprobar si la contraseña es correcta
+  if (existencia) {
+    let resultado = usuarios.lista.filter(x => x.nombre === nombreUsuario)
+    let contrasena = resultado[0].password
+
+    if (contrasena === passwordUsuario) {
+      let color = resultado[0].color || '#ff0000'
+      let nombre = resultado[0].nombre
+      let id = resultado[0].id
+
+      res.json({
+        resultado: 'contraseña correcta',
+        candado: true,
+        id, nombre, color
+      })
+
+    } else {
+      res.json({
+        resultado: 'contraseña incorrecta',
+        candado: false
+      })
+    }
+
+  } else {
+    res.json({
+      resultado: 'usuario no registrado',
+      candado: false
+    })
+  }
+
+})
+
+//CAMBIAR COLOR DE USUARIO//////////////////////////////////////////////////
+server.post('/guardarColor', (req, res) => {
+  let idUser = req.body.id
+  let colorUser = req.body.color
+
+  existencia = usuarios.lista.some(x => x.id === idUser)
+
+  if (existencia) {
+    let indice = usuarios.lista.findIndex(x => x.id === idUser)
+    usuarios.lista[indice].color = colorUser
+
+    fs.writeFileSync('./baseDatos/usuarios.json', JSON.stringify(usuarios))
+
+    res.send('Color del usuario cambiado')
+  } else {
+    res.send('No existe el usuario')
+  }
+})
+
 ////////////////////////////////////////////////////////////////////
 server.use(express.static('public'))
 
